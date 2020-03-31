33a61b6a8ddf601aecd433c8df863826325e408e
juicecultus <juicecultus@gmail.com>
2020-03-31 22:31:20 +0100

    implement signup validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { signup } = require('../controllers/auth');
+//validators
+const { runValidation } = require('../validators');
+const { userSignupValidator } = require('../validators/auth');
 
-router.post('/signup', signup);
+router.post('/signup', userSignupValidator, runValidation, signup);
 
 module.exports = router;
