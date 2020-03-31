e09a8bbc08cc53df534b430de9dd76a06ffe03d9
juicecultus <juicecultus@gmail.com>
2020-03-31 22:53:48 +0100

    update user schema with auth virtual fields & methods

--- models/user.js
+++ models/user.js
@@ -53,4 +53,42 @@ const userSchema = new mongoose.Schema(
   { timestamp: true }
 );
 
+userSchema
+  .virtual('password')
+  .set(function(password) {
+    //create temp var called _password
+    this._password = password;
+
+    //generate salt
+    this.salt = this.makeSalt();
+
+    //encrypt password
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+userSchema.methods = {
+  authenticate: function(plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+
+  ecryptPassword: function(password) {
+    if (!password) return '';
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex');
+    } catch (error) {
+      return '';
+    }
+  },
+
+  makeSalt: function() {
+    return Math.round(new Date().valueOf() * Math.random()) + '';
+  }
+};
+
 module.exports = mongoose.model('User', userSchema);
